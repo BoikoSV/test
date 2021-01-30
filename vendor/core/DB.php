190d8ee971d0db1f9c5967f12fbb466a4d8b5374
BoykoYV <boikosmail2@gmail.com>
2021-01-30 20:58:04 +0200

    Написал клас базы данных. Добавил метод getAllPosts который возвращает все записи из таблицы

---
+++ vendor/core/DB.php
@@ -0,0 +1,65 @@
+<?php
+namespace vendor\core;
+/**
+ * Клас доступа к базе данных. Паттерн одиночка
+ * Class DB
+ * @package vendor\core
+ */
+
+class DB
+{
+
+    private static $instance = null;
+    private $host = 'localhost';
+    private $db = 'blog';
+    private $user = 'root';
+    private $pass = 'root';
+    public $pdo;
+
+
+    /**
+     * В конструкторе устанавливаются все настройки подключения
+     * DB constructor.
+     */
+    private function __construct() {
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=utf8";
+        $opt = [
+            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+            \PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $this->pdo = new \PDO($dsn, $this->user, $this->pass, $opt);
+    }
+
+    /**
+     * Согласно паттерну одиночка, нужно получать только единственный экземляр обьекта.
+     * Эта возможность реализована в этом методе
+     * @return DB|null
+     */
+    public static function getInstance()
+    {
+        if (null === self::$instance)
+        {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    /**
+     * Метод для получения всех записей с базы данных из двух таблиц. users и posts.
+     *
+     * @return array
+     */
+    public function getAllPosts(){
+        $sql = 'SELECT * FROM users JOIN posts ON users.id = posts.user_id';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        return $result;
+    }
+    public function getOneUsersPosts($quantity, $user){
+
+    }
+
+    private function __clone() {}
+}

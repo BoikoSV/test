39899c159072f78d39e1ec9690dff65f3afce392
BoykoYV <yulchitaiboldireva@gmail.com>
2021-01-30 16:24:26 +0200

    Добавил класс Router, в котором определил метод parseQuery, для определения данных о текущем запросе.

--- public/index.php
+++ public/index.php
@@ -2,21 +2,30 @@
 define("ROOT", $_SERVER['DOCUMENT_ROOT']);
 use vendor\core\Router;
 
+/**
+ * Функция автозагрузки классов
+ */
 spl_autoload_register(function ($class) {
     $class = str_replace('\\', '/', $class);
     $file = ROOT . '/' . $class . '.php';
-    var_dump($file);
     if (file_exists($file)){
         require_once $file;
     }
 });
 
 /**
- * Все доступные маршруты
+ * Получение строки запроса с адрессной строки браузера
  */
-Router::add(['#^$/?#', ['controller' => 'main', 'model' => 'index']]);
+$query = $_SERVER['REQUEST_URI'];
 
 
+/**
+ * Сюда будут писаться все маршруты которые будут обрабатываться приложением.
+ * В начале идет шаблон регулярного выражения, которое будет характеризовать,
+ * строку запроса и масив с названием класса и метода который обработает этот запрос.
+ */
+Router::add(['#^(user/\d+)?$#i', ['controller' => 'Main', 'method' => 'index']]);
 
 
+Router::parseQuery($query);
 

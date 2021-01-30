811aa788ed36211809cbd421103e9dc950613864
BoykoYV <boikosmail2@gmail.com>
2021-01-30 23:18:47 +0200

    В класс DB добавил метод который достает посты одного пользователя.

--- vendor/core/DB.php
+++ vendor/core/DB.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace vendor\core;
 /**
  * Клас доступа к базе данных. Паттерн одиночка
  * Class DB
  * @package vendor\core
  */
-
 class DB
 {
 
@@ -21,12 +21,13 @@ class DB
      * В конструкторе устанавливаются все настройки подключения
      * DB constructor.
      */
-    private function __construct() {
+    private function __construct()
+    {
         $dsn = "mysql:host=$this->host;dbname=$this->db;charset=utf8";
         $opt = [
-            \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-            \PDO::ATTR_EMULATE_PREPARES   => false,
+            \PDO::ATTR_EMULATE_PREPARES => false,
         ];
         $this->pdo = new \PDO($dsn, $this->user, $this->pass, $opt);
     }
@@ -38,8 +39,7 @@ class DB
      */
     public static function getInstance()
     {
-        if (null === self::$instance)
-        {
+        if (null === self::$instance) {
             self::$instance = new self();
         }
         return self::$instance;
@@ -50,16 +50,42 @@ class DB
      *
      * @return array
      */
-    public function getAllPosts(){
+    public function getAllPosts()
+    {
         $sql = 'SELECT * FROM users JOIN posts ON users.id = posts.user_id';
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll();
         return $result;
     }
-    public function getOneUsersPosts($quantity, $user){
 
+    /**
+     * Метод извлечения из таблицы записей одного пользователя.
+     * В параметры передается id из таблицы users лимит значений и возвращается массив.
+     * @param $user
+     * @param $quantity
+     * @return array
+     */
+    public function getOneUsersPosts($user, $quantity)
+    {
+        $sql = "SELECT users.id as user_id,
+                    users.first_name as first_name,
+                    users.last_name as last_name,
+                    posts.title as title,
+                    substring(posts.body, 1, 80) as text
+                    FROM users JOIN posts ON user_id = posts.user_id 
+                    WHERE user_id = :user LIMIT :quantity";
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->execute([
+            'user' => $user,
+            'quantity' => $quantity
+        ]);
+        $result = $stmt->fetchAll();
+        return $result;
     }
 
-    private function __clone() {}
+    private function __clone()
+    {
+    }
 }

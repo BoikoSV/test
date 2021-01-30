c9c30cd13c6ab3069b2d98f225d63e9613e49a38
BoykoYV <boikosmail2@gmail.com>
2021-01-31 00:43:27 +0200

    Добавил метод который выводит посты одного пользователя

--- app/models/Post.php
+++ app/models/Post.php
@@ -6,9 +6,25 @@ use vendor\core\DB;
 class Post
 {
     public $table_name = 'Posts';
+    public $quantity = 5;
 
+    /**
+     * Метод модели который выводит посты всех пользователей
+     * @return array
+     */
     public function getAll()
     {
        return DB::getInstance()->getAllPosts();
+    }
+
+    /**
+     * Метод фильтрует посты по одному пользователю
+     * @param $user
+     * @return array
+     */
+    public function getOneUserPosts($user){
+
+        return DB::getInstance()->getOneUsersPosts($user, $this->quantity);
+
     }
 }

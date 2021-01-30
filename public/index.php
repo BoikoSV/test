9840b0fae336c8da6ee0d8714283a066dc25856e
BoykoYV <yulchitaiboldireva@gmail.com>
2021-01-30 15:23:03 +0200

    Добавил автозагрузчик

--- public/index.php
+++ public/index.php
@@ -1,3 +1,22 @@
 <?php
-var_dump($_SERVER);
-echo 'hello';
+define("ROOT", $_SERVER['DOCUMENT_ROOT']);
+use vendor\core\Router;
+
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+    $file = ROOT . '/' . $class . '.php';
+    var_dump($file);
+    if (file_exists($file)){
+        require_once $file;
+    }
+});
+
+/**
+ * Все доступные маршруты
+ */
+Router::add(['#^$/?#', ['controller' => 'main', 'model' => 'index']]);
+
+
+
+
+

b5ade4ec0a7c29a4d51734627e5265b2e77b4d7c
BoykoYV <boikosmail2@gmail.com>
2021-01-31 15:17:33 +0200

    Добавил CurlController

---
+++ app/controllers/CurlController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace app\controllers;
+
+use vendor\core\Controller;
+use vendor\core\DB;
+use app\models\CurlModel;
+
+/**
+ * Контроллер Curl
+ * Class CurlController
+ * @package app\controllers
+ */
+class CurlController extends Controller
+{
+    /**
+     * Обьекты json преобразует в массив и передает базе данных
+     */
+    public function recordingData(){
+        $curlModel = new CurlModel();
+        $arr = $curlModel->getData();
+        $db = DB::getInstance();
+        foreach ($arr as $value){
+            $db->insertData($value);
+
+        }
+
+    }
+}
